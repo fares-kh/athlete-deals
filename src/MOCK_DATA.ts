b738f51e0c1d1b27168ee2f576497de634531400
fares-kh <fares.khaled22@hotmail.com>
2025-01-12 16:47:19 +0000

    add mock prods.

---
+++ src/MOCK_DATA.ts
@@ -0,0 +1,50 @@
+export const response = {
+    PROVIDER_A: {
+        product: {
+            name: "Peanut Butter",
+            price: "£6.49"
+        }
+    },
+    PROVIDER_B: {
+        product: {
+            name: "Peanut Butter",
+            price: "£5.72"
+        }
+    },
+    PROVIDER_C: {
+        product: {
+            name: "Peanut Butter",
+            price: "£3.49"
+        }
+    },
+    PROVIDER_E: {
+        product: {
+            name: "Peanut Butter",
+            price: "£9.72"
+        }
+    },
+    PROVIDER_F: {
+        product: {
+            name: "Peanut Butter",
+            price: "£5.49"
+        }
+    },
+    PROVIDER_G: {
+        product: {
+            name: "Peanut Butter",
+            price: "£4.72"
+        }
+    },
+    PROVIDER_H: {
+        product: {
+            name: "Peanut Butter",
+            price: "£8.29"
+        }
+    },
+    PROVIDER_I: {
+        product: {
+            name: "Peanut Butter",
+            price: "£10.72"
+        }
+    }
+}
--- src/Query.tsx
+++ src/Query.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import { response as MOCK_PRODUCTS } from './MOCK_DATA'
 
 interface Product {
   product: {
@@ -21,8 +22,11 @@ function App() {
   React.useEffect(() => {
     const fetchProductData = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/scrape?query=${query}`)
-        setProductData(response.data)
+        // uncomment to use online queries
+        // const response = await axios.get(`http://localhost:5000/scrape?query=${query}`)
+        // setProductData(response.data)
+        const response = MOCK_PRODUCTS
+        setProductData(response)
         console.log("response")
         console.log(response)
         setLoading(false)
@@ -45,7 +49,13 @@ function App() {
       ) : productData ? (
         <div>
           {Object.keys(productData).map((key) => (
-            <div key={key}>
+            <div key={key}
+            // move to styled components or external style sheet
+            style={{
+                display: 'grid',
+                gridTemplateColumns: '2fr 1fr 1fr',
+                width: '600px',
+            }}>
               <h2>{key}</h2>
               <p><strong>Name:</strong> {productData[key].product.name}</p>
               <p><strong>Price:</strong> {productData[key].product.price}</p>
